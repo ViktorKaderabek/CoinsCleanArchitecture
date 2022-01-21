eb9959ae175b78582a58ffaf74b0a7333adfbb79
viktorkaderabek <viktorkaderabekpc@seznam.cz>
2022-01-21 09:12:15 +0100

    Projekt

--- app/src/main/java/com/example/coins_clean_architecture/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/example/coins_clean_architecture/data/repository/CoinRepositoryImpl.kt
@@ -1,18 +1,27 @@
 package com.example.coins_clean_architecture.data.repository
 
+import android.util.Log
+import com.example.coins_clean_architecture.common.Resource
 import com.example.coins_clean_architecture.data.remote.CoinPaprikaApi
 import com.example.coins_clean_architecture.data.remote.dto.CoinsDtoItem
+import com.example.coins_clean_architecture.data.remote.dto.toCoins
+import com.example.coins_clean_architecture.domain.model.Coins
 import com.example.coins_clean_architecture.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import retrofit2.HttpException
+import java.io.IOException
 
-class CoinRepositoryImpl(
-    private val api : CoinPaprikaApi
-) : CoinRepository{
+class CoinRepositoryImpl() : CoinRepository, KoinComponent{
+
+    private val api : CoinPaprikaApi by inject()
 
     override suspend fun getCoins(): List<CoinsDtoItem> {
        return api.getCoins()
     }
+
 
 
 }
--- app/src/main/java/com/example/coins_clean_architecture/presentation/CoinsListState.kt
+++ app/src/main/java/com/example/coins_clean_architecture/presentation/CoinsListState.kt
@@ -6,6 +6,6 @@ import com.example.coins_clean_architecture.domain.model.Coins
 
 data class CoinsListState(
     val isLoading : Boolean = false,
-    var coins : List<Coins> = emptyList(),
+    val coins : List<Coins> = emptyList(),
     val error : String? = null
 )
